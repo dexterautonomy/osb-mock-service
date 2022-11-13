a0941822f24550f57782224fa153cf0842ed58a7
brightchi <chinedu.ezeigbo@mtn.com>
2022-11-13 19:29:50 +0100

    Update

--- src/main/java/com/techm/devops/controller/ss/MockHuaweiProvisioningController.java
+++ src/main/java/com/techm/devops/controller/ss/MockHuaweiProvisioningController.java
@@ -1,7 +1,15 @@
 package com.techm.devops.controller.ss;
 
+import com.techm.devops.dto.ecw.UpdateCitizenship;
+import com.techm.devops.dto.ecw.UpdateCitizenshipResponse;
+import com.techm.devops.dto.ss.provisioning.SendSyncReqResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/huawei")
 public class MockHuaweiProvisioningController {
+
+    @PostMapping(value = "addsub", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<SendSyncReqResponse> addSub(@RequestBody Object object){
+        log.info("--->> Request: {}", object);
+
+        return new ResponseEntity<>(new SendSyncReqResponse(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "rmvsub", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<SendSyncReqResponse> rmvSub(@RequestBody Object object){
+        log.info("--->> Request: {}", object);
+
+        return new ResponseEntity<>(new SendSyncReqResponse(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "modproduct", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<SendSyncReqResponse> modProd(@RequestBody Object object){
+        log.info("--->> Request: {}", object);
+
+        return new ResponseEntity<>(new SendSyncReqResponse(), HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/techm/devops/controller/ss/MockOCSProvisioningController.java
+++ src/main/java/com/techm/devops/controller/ss/MockOCSProvisioningController.java
@@ -1,7 +1,16 @@
 package com.techm.devops.controller.ss;
 
+import com.techm.devops.dto.ss.ocs.changesubpaymentmode.ChangeSubPaymentModeResultMsg;
+import com.techm.devops.dto.ss.ocs.createsubscriber.CreateSubscriberResultMsg;
+import com.techm.devops.dto.ss.ocs.creditlimitchange.ChangeAcctCreditLimitResultMsg;
+import com.techm.devops.dto.ss.ocs.subdeactivation.SubDeactivationResultMsg;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/ocs")
 public class MockOCSProvisioningController {
 
+    @PostMapping(value = "subdeactivation", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<Object> subdeactivation(@RequestBody Object object){
+        log.info("--->> Request: {}", object);
+
+        return new ResponseEntity<>(new SubDeactivationResultMsg(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "createsubscriber", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<Object> createsubscriber(@RequestBody Object object){
+        log.info("--->> Request: {}", object);
+
+        return new ResponseEntity<>(new CreateSubscriberResultMsg(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "changepayment", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<Object> changepayment(@RequestBody Object object){
+        log.info("--->> Request: {}", object);
+
+        return new ResponseEntity<>(new ChangeSubPaymentModeResultMsg(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "creditlimit", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<Object> creditlimit(@RequestBody Object object){
+        log.info("--->> Request: {}", object);
+
+        return new ResponseEntity<>(new ChangeAcctCreditLimitResultMsg(), HttpStatus.OK);
+    }
 }
